e8a73d1b3957b53e1f6ad75fcf55cc86aec38547
Marko <marko@infonet.ee>
2021-12-09 23:44:43 +0200

    Revert "new changes"

--- app2.js
+++ app2.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const pool = require('./database');
+//const pool = require('./database');
 const cors = require('cors');
 const app = express();
 // register the ejs view engine
@@ -12,23 +12,13 @@ app.use(express.static('Public'));
 app.listen(3000, () => {
     console.log("Server is listening to port 3000")
 });
-app.get('/', async (req, res) => {
-    //res.render('posts');
-    try {
-        console.log("get posts request has arrived");
-        const posts = await pool.query(
-            "SELECT * FROM nodetable"
-        );
-        res.render('posts', { posts: posts.rows });
-    } catch (err) {
-        console.error(err.message);
-    }
-
+app.get('/', (req, res) => {
+    res.render('posts');
 });
 app.get('/posts', async (req, res) => {
     //hetkel kommenteerisin välja andmebaasist fetchimise, kuna andmebaasi pole veel postgres
-    //res.render('posts');
-    try {
+    res.render('posts');
+    /* try {
         console.log("get posts request has arrived");
         const posts = await pool.query(
             "SELECT * FROM nodetable"
@@ -36,7 +26,7 @@ app.get('/posts', async (req, res) => {
         res.render('posts', { posts: posts.rows });
     } catch (err) {
         console.error(err.message);
-    }
+    } */
 });
 app.get('/singlepost/:id', async (req, res) => {
     try {
@@ -86,7 +76,7 @@ app.post('/posts', async (req, res) => {
         const post = req.body;
         console.log(post);
         const newpost = await pool.query(
-            "INSERT INTO nodetable(title, body, time) values ($1, $2, $3) RETURNING*", [post.titlex, post.bodyx, post.create_timex]
+            "INSERT INTO nodetable(title, body, create_time) values ($1, $2, $3) RETURNING * ", [post.titlex, post.bodyx, post.create_timex]
         );
         res.redirect('posts');
     } catch (err) {
@@ -98,17 +88,4 @@ app.get('/addnew', (req, res) => {
 });
 app.use((req, res) => {
     res.status(404).render('404');
-});
-app.delete('/posts/:id', async (req, res) => {
-    try {
-        console.log(req.params);
-        const { id } = req.params;
-        console.log("delete a post request has arrived");
-        const deletepost = await pool.query(
-            "DELETE FROM nodetable WHERE id = $1", [id]
-        );
-        res.redirect('posts');
-    } catch (err) {
-        console.error(err.message);
-    }
 });
